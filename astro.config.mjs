d3919ee8fc380f5d6b7dab793008e7e2c6f72401
JoIbr <javohiribr@icloud.com>
2026-05-31 15:51:59 +0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: 'server',
+  adapter: netlify()
 });
